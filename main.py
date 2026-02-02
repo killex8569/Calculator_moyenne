77dec97d9e25987dde7b3fbfa014feb0bea45207
killex8569 <alexandreanette@gmail.com>
2026-02-02 22:02:32 +0100

    Feat: Seg semestre

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import csv
 
-stats = {}
+stats = {} # On déclare un dictionnaire plus simple pour les données
 
 with open("mes_notes.csv", encoding="latin-1", newline="") as f:
-    for r in csv.DictReader(f, delimiter=";"):
-        cours = r["Cours"]
+    for r in csv.DictReader(f, delimiter=";"): # Lecture ligne par ligne du CSV, csv.DictReader permet de lire la première ligne comme en-tête
+        # Extraction des champs (manipulable avec des variables)
+        cours = r["Cours"] 
         coef = float(r["Coefficient"].replace(",", "."))
         note = float(r["Note"].replace(",", "."))
+        semestre = r["Semestre"]
 
-        if cours not in stats:
-            stats[cours] = {"sum": 0.0, "coef": 0.0}
+        if semestre not in stats: # Si le semestre n'éxiste pas encore --> On créer un dictionnaire vide
+            stats[semestre] = {}
 
-        stats[cours]["sum"] += note * coef
-        stats[cours]["coef"] += coef
+        if cours not in stats[semestre]:
+            stats[semestre][cours] = {"sum": 0.0, "coef": 0.0} # Chaque cours stock le total de note + coef
 
-for cours, v in stats.items():
-    print(cours, ":", v["sum"] / v["coef"])
+        stats[semestre][cours]["sum"] += note * coef
+        stats[semestre][cours]["coef"] += coef
+
+for semestre, cours_2 in stats.items(): # Parcour des resultats
+    print(semestre, "\n")
+    for cours, v in cours_2.items(): # Parcour uniquement les cours dans le semestre actuelle et v vaut sum et coef
+        print(cours, ":", v["sum"] / v["coef"]) 
