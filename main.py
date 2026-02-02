391698fb3f318ffd5db5040560e7367b9e6f3324
killex8569 <alexandreanette@gmail.com>
2026-02-02 16:04:22 +0100

    Feat: calc moy per module

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import csv
 
+stats = {}
+
 with open("mes_notes.csv", encoding="latin-1", newline="") as f:
     for r in csv.DictReader(f, delimiter=";"):
+        cours = r["Cours"]
         coef = float(r["Coefficient"].replace(",", "."))
         note = float(r["Note"].replace(",", "."))
-        print(r["Cours"], coef, note)
 
+        if cours not in stats:
+            stats[cours] = {"sum": 0.0, "coef": 0.0}
 
-# Il vas falloir créer deux variables, si elles sont identiques, alors on vas pouvoirs calculer et ajouter les coef, sinon on continue à lire
+        stats[cours]["sum"] += note * coef
+        stats[cours]["coef"] += coef
 
-
+for cours, v in stats.items():
+    print(cours, ":", v["sum"] / v["coef"])
